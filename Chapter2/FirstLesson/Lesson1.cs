6d8c0065188d1a439e2f19534194dee5c56c95ad
yu-mori <standy.by555@gmail.com>
2019-03-28 23:11:36 +0900

    push chapter2

---
+++ Chapter2/FirstLesson/Lesson1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Person p = new Person ();//これがインスタンス
+        p.firstname = "太郎";
+        p.lastname = "山田";
+        Debug.Log (p.GetFullName ("★"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+class Person{
+    public string firstname;
+    public string lastname;
+
+    public string GetFullName (string separator) {
+        return this.lastname + separator + this.firstname;
+    }
+}

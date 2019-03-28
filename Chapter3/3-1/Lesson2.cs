4e1ec3b996c3fd03652db3ee0306a090355eff5c
yu-mori <standy.by555@gmail.com>
2019-03-28 23:31:14 +0900

    push chapter3-1

---
+++ Chapter3/3-1/Lesson2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if文
+        // int a = 9;
+        // if (a >= 3 && a <= 7) {
+        //     Debug.Log ("3～7の間");
+        // }
+        // if (a < 3 || a > 7){
+        //     Debug.Log ("3～7の間ではない");
+        // }
+
+        //switch文
+        string menu = "save";
+
+        switch (menu) {
+        case "save" :
+            Debug.Log ("保存します");
+            break;
+        case "open" :
+            Debug.Log ("開きます");
+            break;
+        case "print" :
+            Debug.Log ("印刷します");
+            break;
+        default :
+            Debug.Log ("一致する機能がありません");
+            break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
